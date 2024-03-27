b8141be74efe187d1fdc52f3ba65a82f93d8711e
Deyi Li <deyi.li2015@gmail.com>
2024-03-27 13:54:14 -0700

    controlled search component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import { Navbar } from 'react-bootstrap';
-
+import React from "react";
+import { Navbar, Container } from "react-bootstrap";
+const navbarStyle = {
+  backgroundColor: "lightblue",
+};
 const Header = (props) => {
-    return (
-        <Navbar bg="light" data-bs-theme="light">
-          <Navbar.Brand href="/">{props.title}</Navbar.Brand>
-      </Navbar>
-    )
+  return (
+    <Navbar style={navbarStyle} data-bs-theme="light">
+      <Container>
+        <Navbar.Brand href="/">{props.title}</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
