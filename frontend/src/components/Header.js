d398b0fc0fce18ff8bf360b847e2f1aa00e6578b
Deyi Li <deyi.li2015@gmail.com>
2024-03-28 16:38:21 -0700

    add logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Navbar, Container } from 'react-bootstrap';
+import { ReactComponent as Logo } from '../images/logo.svg';
+
 const navbarStyle = {
   backgroundColor: 'lightblue',
 };
@@ -7,7 +9,7 @@ const Header = (props) => {
   return (
     <Navbar style={navbarStyle} data-bs-theme="light">
       <Container>
-        <Navbar.Brand href="/">{props.title}</Navbar.Brand>
+        <Logo style={{ maxWidth: '12rem', maxHeight: '2rem' }} />
       </Container>
     </Navbar>
   );
