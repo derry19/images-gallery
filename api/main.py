b032d090e9c0181b97b840fed372a86e638c9fa8
Deyi Li <deyi.li2015@gmail.com>
2024-03-30 15:36:00 -0700

    formatter extension added for python

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-import os 
+import os
 import requests
 from flask import Flask, request
 from dotenv import load_dotenv
@@ -6,32 +6,29 @@ from flask_cors import CORS
 
 load_dotenv(dotenv_path="./.env.local")
 
-UNSPLASH_URL='https://api.unsplash.com/photos/random'
-UNSPLASH_KEY=os.environ.get("UNSPLASH_KEY", "")
-DEBUG=bool(os.environ.get("FLASK_DEBUG", True))
+UNSPLASH_URL = "https://api.unsplash.com/photos/random"
+UNSPLASH_KEY = os.environ.get("UNSPLASH_KEY", "")
+DEBUG = bool(os.environ.get("FLASK_DEBUG", True))
 
 if not UNSPLASH_KEY:
-  raise EnvironmentError("Authorization key absent in .env.local")
+    raise EnvironmentError("Authorization key absent in .env.local")
 
 app = Flask(__name__)
 CORS(app)
 
 app.config["FLASK_DEBUG"] = DEBUG
 
-@app.route('/new-image')
-def new_image():
-  word = request.args.get("query")
 
-  headers = {
-    "Accept-Version": "v1",
-    "Authorization": "Client-ID " + UNSPLASH_KEY
-  }
-  params = {
-    "query": word
-  }
-  response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
-  data = response.json()
-  return data
+@app.route("/new-image")
+def new_image():
+    word = request.args.get("query")
+
+    headers = {"Accept-Version": "v1", "Authorization": "Client-ID " + UNSPLASH_KEY}
+    params = {"query": word}
+    response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
+    data = response.json()
+    return data
+
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0", port=5050)
+    app.run(host="0.0.0.0", port=5050)
